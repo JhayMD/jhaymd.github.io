7af48fa3fe2567f0b650f26bdd5c3cd78d691153
Jevon <67438479+JhayMD@users.noreply.github.com>
2024-02-23 07:58:52 -0800

    Add files via upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+let contactMeBtn = document.getElementById('contactMe');
+let contactMecloseBtn = document.getElementById('closeBtn');
+
+
+function contactMe() {
+    contactMeBtn.style.display = 'block';
+}
+
+function closeBtn() {
+    contactMeBtn.style.display = 'none';
+}
